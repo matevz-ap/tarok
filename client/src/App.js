55f5f64ebaacf76ab5798b544981d2035fecb5e6
mtx13 <matepeklaj@gmail.com>
2021-07-07 20:06:09 +0200

    fix 2

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,10 @@ import Home from "./components/Home";
 import SideBar from "./components/SideBar";
 import NavBar from "./components/NavBar";
 
-21721
 const ENDPOINT =
   process.env.NODE_ENV === 'production'
-    ? "http://localhost:21721"
-    : "http://localhost:3001";
+    ?  window.location.hostname
+    : 'https://localhost:3001';
 
 const socket = socketIOClient(ENDPOINT);
 
